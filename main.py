e4ee34573d99cca009b4bcd20f395544b2b5dc54
Max Leibowitz <403089@bsd48.org>
2020-08-06 20:35:17 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import json
+import time
+import subprocess
+from discord.ext import commands
+
+supersecretstuff = json.loads(open("secrets.json").read())
+
+
+client = commands.Bot(command_prefix="?")
+
+
+@client.command()
+async def status(ctx: commands.Context):
+    await ctx.send(str(subprocess.run("papermc status | grep Status", stdout=subprocess.PIPE, shell=True).stdout, "utf-8"))
+
+client.run(supersecretstuff)
