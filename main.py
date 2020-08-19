d649a5bf89c545d4f9a352bcf38d3f3d135e1488
Mewtastic <42871562+Mewtastic@users.noreply.github.com>
2020-08-19 13:27:36 -0700

    Remove checks

--- main.py
+++ main.py
@@ -20,24 +20,16 @@ async def status(ctx: commands.Context):
 @commands.has_any_role(733773680147955833, 733558343209320470, 741095502791311410)
 async def verify(ctx: commands.Context, member: discord.Member, username=None):
     """Verify a member"""
-    print('check point a')
     if isinstance(member, discord.Member):
-        print('check point b')
         guild: discord.Guild = ctx.guild
-        print('check point alpha')
         await member.add_roles(guild.get_role(733558721736736810))
-        print('check point beta')
         await member.remove_roles(guild.get_role(734260573453418499))
-        print('check point gamma')
         if username != None:
-            print('check point c')
             subprocess.run("papermc command whitelist add " + str(username), shell=True).stdout, "utf-8"
             await ctx.send(f"```Verified: Added {username} to whitelist!```")
         else:
-            print('check point d')
             await ctx.send(f"```Verified: No username provided!```")
     else:
-        print('check point e')
         await ctx.send(f"```Critical Error: Discord member does not exist!```")
 
 
